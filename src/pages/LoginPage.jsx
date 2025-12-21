86c08378d3bda0cbd6b5c992fdde9b951cb1e35a
Brian Munyoki <brianmunyoki168@gmail.com>
2025-12-21 20:08:43 +0300

    adding states for passwords

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 
 function LoginPage(){
+const [username,setUsername]=useState("");
+const [password,setPassword]=useState("");
     return(
    <div>
     <LoginCard />
@@ -9,56 +11,83 @@ function LoginPage(){
 
 export default LoginPage;
 
-function LoginCard(){
+function LoginCard({username,setUsername,password,setPassword}){
     return(
         <div>
         <BrandHeader/>
         <RoleSwitcher/>
-        <LoginForm/>
+        <LoginForm username={username} setUsername={setUsername} password={password} setPassword={setPassword}/>
         <Divider/>
         <GoogleButton/>
         <SignupPrompt/>
         </div>
-    )
+    );
 }
 
 function BrandHeader(){
     return(
         <div>BrandHeader</div>
-    )
+    );
 }
 
 function RoleSwitcher(){
     return(
         <div>RoleSwitcher</div>
-    )
+    );
 }
 
 function LoginForm(){
     return(
         <div>
-        <PhoneInput />
+        <UsernameInput />
         <PasswordInput />
         <ForgotPasswordLink />
         <LoginButton />
         </div>
-    )
+    );
 }
 
 function Divider(){
     return (
         <div>Divider</div>
-    )
+    );
 }
 
 function GoogleButton(){
     return(
         <div>GoogleButton</div>
-    )
+    );
 }
 
 function SignupPrompt(){
     return(
-        <div>SignupPrompt</div>
-    )
+        <div>
+            <p>Don't have an account</p>
+            <button type="button">Sign up</button>
+        </div>
+    );
+}
+
+function UsernameInput(){
+    return(
+        <div>usernameInput</div>
+    );
+}
+
+function PasswordInput(){
+    return(
+        <div>PasswordInput</div>
+    );
+}
+
+function ForgotPasswordLink(){
+    return(
+        <div>ForgotPasswordLink</div>
+    );
+}
+
+function LoginButton(){
+    return(
+        <div>LoginButton</div>
+    );
 }
