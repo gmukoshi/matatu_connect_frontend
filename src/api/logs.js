636f6a6c3b5360b8f37977659fdfd3b43ea4091b
Kevin <kevinkoech007@gmail.com>
2026-01-16 11:53:23 +0300

    feat: Add fetchLogs API function

--- src/api/logs.js
+++ src/api/logs.js
@@ -1,4 +1,8 @@
 import axiosInstance from './apiClient';
 
 // Submit a daily log
+// Submit a daily log
 export const submitDriverLog = (data) => axiosInstance.post('/logs/', data);
+
+// Fetch logs (Manager)
+export const fetchLogs = () => axiosInstance.get('/logs/');
